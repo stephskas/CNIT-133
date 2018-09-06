4d1d635ef8aa1ef181c6067891a037c9e3730513
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-06 01:32:23 -0700

    Add files via upload

--- h2_part2-1.js
+++ h2_part2-1.js
@@ -3,7 +3,6 @@ var n1 = parseInt(document.getElementById('n1').value);
 var n2 = parseInt(document.getElementById('n2').value); 
 var n3 = parseInt(document.getElementById('n3').value); 
 // Convert inputed string into numbers and calculate sum of the 3 numbers
-alert(n3);
 var sum = n1 + n2 + n3;
 // Calculate average of the 3 numbers
 var numbers = [n1, n2, n3];
