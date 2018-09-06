80524d35b6efdaa244ef695c3a6e914263cb8dbd
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-05 19:15:02 -0700

    Add files via upload

--- h2_part1.js
+++ h2_part1.js
@@ -1,2 +1,18 @@
+//Write all letters in pairs and in strong (bold) using document.write statements
+// Write the letters ab in font family of Times New Roman and the color red 
+document.write("<div style='text-align:center;font-weight:bold'><span style='color:#ff0000;font-family:\"times new roman\",times,serif;'>ab</span>");
 
-document.getElementById("content").innerHTML = "<span style='color:#ff0000;font-family:\"times new roman\",times,serif;'>ab</span><span style='color:#3366ff;font-family:arial,helvetica,sans-serif;'> 12</span><span style='color:#008000;font-family:\"impact; font-style: italic;'> 34</span>"
+ // Write the numbers 12 in font family of Arial and the color blue           
+document.write("<span style='color:#3366ff;font-family:arial,helvetica,sans-serif;'> 12</span>");
+
+ // Write the numbers 34 in font family of Impact, the color green, and emphasized (in italics)           
+document.write("<span style='color:#008000;font-family:impact;font-style: italic;'> 34</span></div>");
+
+
+/*
+Also could display using this script:
+document.getElementById("content").innerHTML = "<span style='color:#ff0000;font-family:\"times new roman\",times,serif;'>ab</span><span style='color:#3366ff;font-family:arial,helvetica,sans-serif;'> 12</span><span style='color:#008000;font-family:impact;font-style:italic;'> 34</span>"
+*/
+
+
+
