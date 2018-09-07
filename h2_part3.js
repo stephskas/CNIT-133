cb572b6e87fab472e9e0868a7160324055881cea
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-06 23:33:52 -0700

    Add files via upload

--- h2_part3.js
+++ h2_part3.js
@@ -1,9 +1,11 @@
+
 //  Store the 5 inputed numbers in variables
-var n1 = parseInt(document.getElementById("n1").value); 
-var n2 = parseInt(document.getElementById("n2").value); 
-var n3 = parseInt(document.getElementById("n3").value); 
-var n4 = parseInt(document.getElementById("n4").value); 
-var n5 = parseInt(document.getElementById("n5").value); 
+var n1 = parseInt(document.getElementById("num1").value); 
+var n2 = parseInt(document.getElementById("num2").value); 
+var n3 = parseInt(document.getElementById("num3").value); 
+var n4 = parseInt(document.getElementById("num4").value); 
+var n5 = parseInt(document.getElementById("num5").value); 
+
 // Count the number of the negative numbers, the number of positive numbers, and the number of zeros
 var numbers = [n1, n2, n3, n4, n5];
 
@@ -42,11 +44,11 @@ $(document).ready(function(){
         $("textarea#positiveNumbers").fadeTo("slow", 0);
         $("textarea#negativeNumbers").fadeTo("slow", 0);
         $("textarea#zeros").fadeTo("slow", 0);
-        $("#n1").val("")
-        $("#n2").val("")
-        $("#n3").val("")
-        $("#n4").val("")
-        $("#n5").val("")
+        $("#num1").val("")
+        $("#num2").val("")
+        $("#num3").val("")
+        $("#num4").val("")
+        $("#num5").val("")
     })
 })
 
