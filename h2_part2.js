709f4ac4b9906194cf77f65aefb9df1a709c6e2f
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-07 15:41:34 -0700

    Add files via upload

--- h2_part2.js
+++ h2_part2.js
@@ -2,43 +2,51 @@
 var n1 = parseInt(document.getElementById('n1').value); 
 var n2 = parseInt(document.getElementById('n2').value); 
 var n3 = parseInt(document.getElementById('n3').value); 
-// Convert inputed string into numbers and calculate sum of the 3 numbers
+
+// Calculate sum of the 3 numbers
 var sum = n1 + n2 + n3;
 // Calculate average of the 3 numbers
-var numbers = [n1, n2, n3];
 var average = parseInt(sum/numbers.length);
 // Calculate average of the 3 numbers
 var product = parseInt(n1 * n2 * n3);
 // Calculate smallest and largest numbers
-var largest = numbers[0];
-var smallest = numbers[0];
-for (var i = 1; i < numbers.length; i++) {
+// Convert inputed string into numbers and calculate smallest numbers
+var numbers = [n1, n2, n3];
+var smallest = Math.min(...numbers);
+// Calculate largest numbers
+var largest = Math.max(...numbers);
 
-    if (numbers[i] > largest) {
-        largest = numbers[i];
-    }
-    else if(numbers[i] < smallest) {
-        smallest = numbers[i];
-    }
-}
 // Create reference to SUBMIT and RESET buttons
 var submitBtn = document.getElementById('button_submit');
 var resetBtn = document.getElementById('button_reset');
 
+// Create and initialize variables for results
+var sumResult = document.getElementById('sumResult');
+var averageResult = document.getElementById('averageResult');
+var productResult = document.getElementById('productResult');
+var smallestResult = document.getElementById('smallestResult');
+var largestResult = document.getElementById('largestResult');
+
 // Clicking on the SUBMIT Button inserts sum, average, product, smallest number, and largest number into the corresponding input fields
 submitBtn.addEventListener('click', calculate);
 function calculate() {
-document.getElementById.numbers[0].value = sum;
-document.getElementById.numbers[1].value = average;
-document.getElementById.numbers[2].value = product;
-document.getElementById.numbers[3].value = smallest; 
-document.getElementById.numbers[4].value = largest;   
+sumResult.value = sum;
+averageResult.value = average;
+productResult.value = product;
+smallestResult.value = smallest; 
+largestResult.value = largest;   
 }
 
 // Clicking on the RESET Button runs the reset function 
+resetBtn.addEventListener('click', clearResults);
 function clearResults()
 {
-    $("sumResult")[0].val("");
+sumResult.value = "";
+averageResult.value = "";
+productResult.value = "";
+smallestResult.value = ""; 
+largestResult.value = "";   
+}
 }
 /*document.getElementById('button_reset').addEventListener('click', reset);
 function reset() {
