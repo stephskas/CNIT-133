b6c56daf5a584843e1405ab38df6679fe1b65e01
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-08 01:09:17 -0700

    Add files via upload

--- h2_part1.js
+++ h2_part1.js
@@ -3,7 +3,7 @@
 document.write("<div style='text-align:center;font-weight:bold'><span style='color:#ff0000;font-family:\"times new roman\",times,serif;'>ab</span>");
 
  // Write the numbers 12 in font family of Arial and the color blue           
-document.write("<span style='color:#3366ff;font-family:arial,helvetica,sans-serif;'> 12</span>");
+document.write("<span style='color:#3366ff;font-family:arial,sans-serif;'> 12</span>");
 
  // Write the numbers 34 in font family of Impact, the color green, and emphasized (in italics)           
 document.write("<span style='color:#008000;font-family:impact;font-style: italic;'> 34</span></div>");
