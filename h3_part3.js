cbcfaece30cd28bd50152f297369dff36933a3f2
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 13:41:58 -0700

    Add files via upload

--- h3_part3.js
+++ h3_part3.js
@@ -1,20 +1,33 @@
+//Declare variables
+var fTemp = document.getElementById('fTemp').value;
+var cTemp = document.getElementById('fTemp').value;
+// Validate input values to make sure field isn't empty
+function validate() {
+    if (isNaN(fTemp || cTemp || item3v || item4v)){
+    document.getElementById('errorMsg').innerHTML = "<emphasis>Please enter numbers only.";
+    }
+}
 
+// Convert Fahrenheit value to Celsius
 function convertToC() {
     var fahrenheit = parseFloat(document.getElementById('fTemp').value);
     
     var celsius = (fahrenheit - 32) * (5 / 9);
     
+// Display Celsius temperature with one digit after decimal place
     document.getElementById('cTemp').value = celsius.toFixed(1);
     
     return false;
 }
 
+// Convert Celsius value to Fahrenheit
 function convertToF() {
     
     var celsius = parseFloat(document.getElementById('cTemp').value);
     
     var fahrenheit = (celsius * (9 / 5)) + 32;
     
+ // Display Fahrenheit temperature with one digit after decimal place   
     document.getElementById('fTemp').value = fahrenheit.toFixed(1);
     
     return false;
