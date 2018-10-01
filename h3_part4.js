0d35a1da64b283a327717007895a158e83d55cc9
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 19:10:55 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -1,15 +1,16 @@
-
 var game = document.getElementById("game");    
-// Generate numbers
-function generateNumbers() {
-// Generate 2 random numbers
-    var num1 = Math.floor(Math.random() * 10);
-    var num2 = Math.floor(Math.random() * 10);
-// Display question   
-document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
-}
+
 // Multiply the 2 random numbers (this will run when the Check Answer button is clicked)
 function multiply() {
+// Generate numbers
+    function generateNumbers() {
+// Generate 2 random numbers
+        var num1 = Math.floor(Math.random() * 10);
+        var num2 = Math.floor(Math.random() * 10);
+// Display question   
+        document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
+        }
+
     var correctAns = num1 * num2;
 // Provide response if the response is correct
     var response = document.getElementById("response").value;
