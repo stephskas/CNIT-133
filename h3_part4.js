6ae6982ebb752e629b91b33d5b26f23a0fef848f
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 20:39:22 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -6,19 +6,20 @@
 
 // Multiply the 2 random numbers (this will run when the Check Answer button is clicked){
     function checkAnswer() {
-        var x,y,name,a,b,answer;
+        var form, response,name,a,b,answer;
 
-        x=document.getElementById("game") ;
-        y=x.elements["response"].value;
+        form = document.getElementById("game") ;
+        response = form.elements["responseInput"].value;
 
-        if (y == num 1 * num2) {
-            document.getElementById("results").innerHTML= "Very good!";
-            /*alert("Very Good! Click ok if you wish to continue.");
-            window.location.reload();*/
+        if (response == num1 * num2) {
+            /*document.getElementById("results").innerHTML= "Very good!";*/
+            alert("Very Good! Click ok if you wish to continue.");
+            window.location.reload();
 
             } 
-                 else { //document.getElementById("results").innerHTML="No. Please try again.";
-                alert("No. Please try again. Click ok if you wish to continue.")
+                 else { /*document.getElementById("results").innerHTML="No. Please try again.";*/
+                
+                response = "";
                 document.getElementById("game").reset();
             }
 }
