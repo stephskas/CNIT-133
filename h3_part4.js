58f9bdc650cca8b25139240a90dc5c3b30925feb
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 18:21:50 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -8,10 +8,11 @@ function multiply() {
 var num1 = Math.floor(Math.random() * 10);
 var num2 = Math.floor(Math.random() * 10);
 // Display question   
-document.getElementById("question").innerHTML = num1 + " x " + num2 + " =";
+document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
 // Provide response if the response is correct
     if (response == correctAns) {
         var result = document.getElementById("result").innerHTML; result = "Very good! Would you like to play again?";
+        document.getElementById("play").innerHTML = "Play Again"; 
         }
 // Provide response if the response is not correct
     else {
