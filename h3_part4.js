1a6b99b5ac7015350c00f0f0a5f5c170a3a91959
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 18:40:31 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -1,15 +1,18 @@
-// Initialize variables
-    var game = document.getElementById("game");    
-    var correctAns = num1 * num2;
-    var response = document.getElementById("response").value;
-// Multiply the 2 random numbers (this will run when the Check Answer button is clicked)
-function multiply() {
+
+var game = document.getElementById("game");    
+// Generate numbers
+function generateNumbers() {
 // Generate 2 random numbers
-var num1 = Math.floor(Math.random() * 10);
-var num2 = Math.floor(Math.random() * 10);
+    var num1 = Math.floor(Math.random() * 10);
+    var num2 = Math.floor(Math.random() * 10);
 // Display question   
 document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
+}
+// Multiply the 2 random numbers (this will run when the Check Answer button is clicked)
+function multiply() {
+    var correctAns = num1 * num2;
 // Provide response if the response is correct
+    var response = document.getElementById("response").value;
     if (response == correctAns) {
         var result = document.getElementById("result").innerHTML; result = "Very good! Would you like to play again?";
         document.getElementById("play").innerHTML = "Play Again"; 
