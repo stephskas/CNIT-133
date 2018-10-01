c40e6f86614033c87940c4bd737810e8620d96c9
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 20:14:13 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -4,21 +4,21 @@ function generateNumbers() {
     var num1 = Math.floor(Math.random() * 10);
     var num2 = Math.floor(Math.random() * 10);
     document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
-}
+
 // Multiply the 2 random numbers (this will run when the Check Answer button is clicked){
-function checkAnswer() {
-	var answer = parseInt(document.forms["game"].elements["response"].value);
-	var correctAnswer = num1 * num2;	
+        function checkAnswer() {
+            var answer = parseInt(document.forms["game"].elements["response"].value);
+            var correctAnswer = num1 * num2;	
 
-	if (correctAnswer == answer) {
-		document.getElementById('results').innerHTML = ("Very Good! Do you want to play Again?");
-		$("game")[0].reset();		
-	} 
-	else 
-	{	
-		document.getElementById("result").innerHTML = ("No. Please try again.");
-		$("game")[0].reset();
-	}
+            if (correctAnswer == answer) {
+                document.getElementById('results').innerHTML = ("Very Good! Do you want to play Again?");
+                $("game")[0].reset();		
+            } 
+            else 
+            {	
+                document.getElementById("result").innerHTML = ("No. Please try again.");
+                $("game")[0].reset();
+            }
+        }
 }
-
 
