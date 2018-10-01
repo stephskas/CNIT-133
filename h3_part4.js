d16c909aaed803e11d74257144800c68c629361d
Steph <32989597+stephskas@users.noreply.github.com>
2018-09-30 20:17:23 -0700

    Add files via upload

--- h3_part4.js
+++ h3_part4.js
@@ -1,24 +1,24 @@
-var num1, num2;
-function generateNumbers() {
+
 // Generate 2 random numbers
     var num1 = Math.floor(Math.random() * 10);
     var num2 = Math.floor(Math.random() * 10);
     document.getElementById("question").innerHTML = num1 + " x " + num2 + " = ";
 
 // Multiply the 2 random numbers (this will run when the Check Answer button is clicked){
-        function checkAnswer() {
-            var answer = parseInt(document.forms["game"].elements["response"].value);
-            var correctAnswer = num1 * num2;	
+    function checkAnswer() {
+        var x,y,name,a,b,answer;
 
-            if (correctAnswer == answer) {
-                document.getElementById('results').innerHTML = ("Very Good! Do you want to play Again?");
-                $("game")[0].reset();		
+        x=document.getElementById("aForm") ;
+        y=x.elements["name"].value;
+
+        if (y==num1*num2) {
+            //document.getElementById("results").innerHTML="Very good!";
+            alert("Very Good! Click ok if you wish to continue.");
+            window.location.reload();
+
             } 
-            else 
-            {	
-                document.getElementById("result").innerHTML = ("No. Please try again.");
-                $("game")[0].reset();
+                 else { //document.getElementById("results").innerHTML="No. Please try again.";
+                alert("No. Please try again. Click ok if you wish to continue.")
+                document.getElementById("game").reset();
             }
-        }
 }
-
